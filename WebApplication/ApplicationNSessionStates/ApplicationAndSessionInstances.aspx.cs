2239f121b087d6af5c9d746a8c1e057204980a8b
Jayant <asmang913@gmail.com>
2019-08-01 00:04:58 +0530

    Added Session State and Application State variables

---
+++ WebApplication/ApplicationNSessionStates/ApplicationAndSessionInstances.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication.ApplicationNSessionStates
+{
+    public partial class ApplicationAndSessionInstances : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox1.Text = Application["TotalAppInstance"].ToString();
+            TextBox2.Text = Application["TotalSessionInstance"].ToString();
+        }
+    }
+}

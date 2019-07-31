c655e92cd483b59b1bf9193492d64e94c686b96e
Jayant <Jayant@DESKTOP-CB2AQVF>
2019-07-31 21:03:09 +0530

    Add project files.

---
+++ WebApplication/Login/DefaultViewstate.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DefaultViewstate.aspx.cs" Inherits="WebApplication.Login.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <p>
+            Username[ASP.NET server control] :
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        Username[HTML control]:<input id="Text1" type="text" runat="server" />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Click" />
+    </form>
+</body>
+</html>
---
+++ WebApplication/Login/ExplicitViewstate.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication.Login
+{
+    public partial class ExplicitViewstate : System.Web.UI.Page
+    {
+        int ClickCount = 1;
+        protected void Page_Load(object sender, EventArgs e)
+        {            
+            if (!IsPostBack)
+            {
+                TextBox1.Text = "0";
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (ViewState["Clicks"] != null)
+            {
+                ClickCount = (int)ViewState["Clicks"] + 1;
+            }
+            TextBox1.Text = ClickCount.ToString();
+            ViewState["Clicks"] = ClickCount;
+        }
+    }
+}
